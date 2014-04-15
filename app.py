cc1a246a9e0ca4a487c2accc4887c68e1d2c01da
jpbinary <jp@iskyy.com>
2014-04-14 19:31:14 -0500

    added hello.html and updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response, session, redirect, url_for, escape
+import os
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    else:
+        response_cookie = make_response(render_template('hello.html'))
+        response_cookie.set_cookie('username', 'the username')
+        return response_cookie
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+# catch all URLs
+@app.route('/<path:path>')
+def catch_all(path):
+    return('You want path: %s' % path)
+ 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    try:
+        app.secret_key = os.environ['secret_key'] 
+    except KeyError:
+        print "Please set (export) the secret_key environmental variable"
+
     app.run(debug=True)
 
